5d0885ab9c5dcf3ddbcd3c1f34e4a0f08245775d
suleman021494 <shaiksuleman382@gmail.com>
2026-07-09 17:26:59 +0530

    updated title

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ accounts = st.session_state.accounts
 # Title
 # ==========================================
 
-st.title("Shaik Suleman :Bank: Smart Banking Management System")
+st.title("Shaik Suleman :blush: Smart Banking Management System")
 
 st.write(
     "Manage customer accounts using a simple Streamlit application."
